5c470b2f5c14a866ef18b8b7cb9e24285d296445
Ahmed Al-Majidi <67476296+AhmedAl-Majidi@users.noreply.github.com>
2024-08-25 14:28:43 +0300

    update: change rating to stars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from "./components/product-list/product-list.component";
-import { ProductCreateComponent } from "./components/product-create/product-create.component";
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCreateComponent } from './components/product-create/product-create.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ProductListComponent, ProductCreateComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    ProductListComponent,
+    ProductCreateComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'EcoBuy';
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,27 +4,34 @@ import { Product, ProductResponse } from '../../models/product';
 import { FormsModule } from '@angular/forms';
 import { NgForOf } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../../services/cart.service';
 import { Category } from '../../models/category';
 import { CategoryService } from '../../services/category.service';
+import { NgxStarsModule } from 'ngx-stars';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [NgForOf, CommonModule,RouterLink,RouterModule, FormsModule],
+  imports: [
+    NgForOf,
+    CommonModule,
+    RouterLink,
+    RouterModule,
+    FormsModule,
+    NgxStarsModule,
+  ],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-
 export class ProductListComponent implements OnInit {
   // cart service
   id!: number;
   product: any;
   quantity!: number;
-  // 
+  //
 
   products: Product[] = [];
   total!: number;
@@ -33,9 +40,8 @@ export class ProductListComponent implements OnInit {
   // **
   filteredProducts: Product[] = [];
   searchTerm: string = '';
-  selectedCategory!: string ;
-   categories$!: Category[];
-
+  selectedCategory!: string;
+  categories$!: Category[];
 
   constructor(
     private prodcutService: ProductService,
@@ -47,15 +53,15 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit() {
     // Initially display all products
-    this.prodcutService.getProducts().subscribe((data:ProductResponse) => {
+    this.prodcutService.getProducts().subscribe((data: ProductResponse) => {
       this.products = data.products;
-      this.filteredProducts = this.products; 
+      this.filteredProducts = this.products;
     });
 
     // populate this.categories from categoryService
-    this.categoryService.getCategories().subscribe(categories => {
+    this.categoryService.getCategories().subscribe((categories) => {
       this.categories$ = categories;
-    })
+    });
   }
 
   // **
@@ -68,16 +74,16 @@ export class ProductListComponent implements OnInit {
   }
 
   applyFilters(): void {
-  
-
-    this.filteredProducts = this.products.filter(product =>
-      product.title.toLowerCase().includes(this.searchTerm.toLowerCase()) &&
-      (this.selectedCategory ? product.category === this.selectedCategory.toLowerCase() : true)
+    this.filteredProducts = this.products.filter(
+      (product) =>
+        product.title.toLowerCase().includes(this.searchTerm.toLowerCase()) &&
+        (this.selectedCategory
+          ? product.category === this.selectedCategory.toLowerCase()
+          : true)
     );
   }
   // Routing to product details pag
-  viewDetails(id: number): void {   
-    this.router.navigate(['/products', id])
+  viewDetails(id: number): void {
+    this.router.navigate(['/products', id]);
   }
-
 }
