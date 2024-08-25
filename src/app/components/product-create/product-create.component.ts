af766113216e9cfc7fe118ea2033c45c724ea184
Ahmed Al-Majidi <67476296+AhmedAl-Majidi@users.noreply.github.com>
2024-08-25 10:43:24 +0300

    Fix: hamburger button & remaing functions in create-product component

--- src/app/components/product-create/product-create.component.ts
+++ src/app/components/product-create/product-create.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
-import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
@@ -13,7 +18,6 @@ import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angula
 export class ProductCreateComponent {
   productForm: FormGroup;
 
-
   constructor(private fb: FormBuilder, private productService: ProductService) {
     this.productForm = this.fb.group({
       id: [0, Validators.required],
@@ -21,24 +25,26 @@ export class ProductCreateComponent {
       description: ['', Validators.required],
       category: ['', Validators.required],
       price: [0, [Validators.required, Validators.min(0)]],
-      discountPercentage: [0, [Validators.required, Validators.min(0), Validators.max(100)]],
+      discountPercentage: [
+        0,
+        [Validators.required, Validators.min(0), Validators.max(100)],
+      ],
       rating: [0, [Validators.required, Validators.min(0), Validators.max(5)]],
       stock: [0, [Validators.required, Validators.min(0)]],
       brand: ['', Validators.required],
     });
   }
-  addNewProduct() {
+  createNewProduct() {
     if (this.productForm.valid) {
       const newProduct: Product = this.productForm.value;
-
-      this.productService.addProduct(newProduct).subscribe({
+      this.productService.createProduct(newProduct).subscribe({
         next: (response) => {
           alert('Product added successfully:');
           console.log(response);
-        }
+        },
       });
     } else {
-      console.error('Form is invalid');
+      console.error('Form is invalid!');
     }
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,34 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, Observable,  } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Product, ProductResponse } from '../models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   apiUrl = 'https://dummyjson.com/products';
 
   getProductById(id: number): Observable<Product> {
     return this.http.get<any>(`https://dummyjson.com/products/${id}`);
   }
-  
+
   getProducts(): Observable<ProductResponse> {
     return this.http.get(this.apiUrl) as Observable<ProductResponse>;
   }
-  
 
-  addProduct(product: Product): Observable<Product> {
+  createProduct(product: Product): Observable<Product> {
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
-
     return this.http.post<Product>(`${this.apiUrl}/add`, product, { headers });
   }
 
-  // Cart 
+  // Cart
   getSingleProduct(id: Number): Observable<any> {
     // return this._api.getTypeRequest('products/' + id);
     return this.http.get(`${this.apiUrl}/${id}`).pipe(
