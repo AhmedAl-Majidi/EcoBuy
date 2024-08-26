8022d655c98b3349e70ef4f5f1bb5b6338546dcf
Ahmed Al-Majidi <ahmedalmajidi7@gmail.com>
2024-08-26 18:40:54 +0300

    update: make chosing category from list and delete rating field

--- src/app/components/product-create/product-create.component.ts
+++ src/app/components/product-create/product-create.component.ts
@@ -6,33 +6,31 @@ import {
   FormGroup,
   FormBuilder,
   Validators,
+  FormsModule,
 } from '@angular/forms';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-create',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule, NgFor],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css',
 })
-export class ProductCreateComponent {
-  productForm: FormGroup;
+export class ProductCreateComponent  {
+  productForm!: FormGroup;
+  // 
+  selectedCategory!: string;
+  categories$!: Category[];
 
-  constructor(private fb: FormBuilder, private productService: ProductService) {
-    this.productForm = this.fb.group({
-      id: [0, Validators.required],
-      title: ['', Validators.required],
-      description: ['', Validators.required],
-      category: ['', Validators.required],
-      price: [0, [Validators.required, Validators.min(0)]],
-      discountPercentage: [
-        0,
-        [Validators.required, Validators.min(0), Validators.max(100)],
-      ],
-      rating: [0, [Validators.required, Validators.min(0), Validators.max(5)]],
-      stock: [0, [Validators.required, Validators.min(0)]],
-      brand: ['', Validators.required],
-    });
+ 
+
+  constructor(private fb: FormBuilder,
+     private productService: ProductService,
+     private categoryService: CategoryService) {
+   
   }
   createNewProduct() {
     if (this.productForm.valid) {
@@ -47,4 +45,27 @@ export class ProductCreateComponent {
       console.error('Form is invalid!');
     }
   }
+  ngOnInit() {
+    // 
+    this.productForm = this.fb.group({
+      id: [0, Validators.required],
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      category: [this.categories$, Validators.required],
+      price: [0, [Validators.required, Validators.min(0)]],
+      discountPercentage: [
+        0,
+        [Validators.required, Validators.min(0), Validators.max(100)],
+      ],
+      stock: [0, [Validators.required, Validators.min(0)]],
+      brand: ['', Validators.required],
+    });
+
+  // populate this.categories from categoryService
+ this.categoryService.getCategories().subscribe((categories) => {
+   this.categories$ = categories;
+   console.log(this.categories$);
+ });
+}
+ 
 }
