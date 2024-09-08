9301125eef4874e3790f74a061ebe1ef789991dd
Ahmed Al-Majidi <67476296+AhmedAl-Majidi@users.noreply.github.com>
2024-09-08 10:51:21 +0300

    fix: fixing AOS error because of SSR

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,29 +1,33 @@
 import { Component } from '@angular/core';
 import { Product } from '../../models/product';
 import { NgFor } from '@angular/common';
-import { ParoductCardComponent } from "../paroduct-card/paroduct-card.component";
-import { SearchComponent } from "../search/search.component";
-import * as AOS from 'aos';
+import { ParoductCardComponent } from '../paroduct-card/paroduct-card.component';
+import { SearchComponent } from '../search/search.component';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [
-    NgFor,
-    ParoductCardComponent,
-    SearchComponent
-],
+  imports: [NgFor, ParoductCardComponent, SearchComponent],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   filteredProducts: Product[] = [];
-  
-  handleFilteredProductsEmitter(data: Product[]){
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  handleFilteredProductsEmitter(data: Product[]) {
     this.filteredProducts = data;
   }
 
-  ngOnInit(){
-    AOS.init();   
+  // Check for Browser Context: Ensure that the code that accesses the document object is only executed in the browser
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Safe to access document here
+      AOS.init();
+    }
   }
 }
