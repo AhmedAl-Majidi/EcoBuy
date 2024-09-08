80ecbeece519631a58c576ccb7bfad0d290883ad
Ahmed Al-Majidi <67476296+AhmedAl-Majidi@users.noreply.github.com>
2024-09-08 10:35:53 +0300

    update: implementing lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,10 @@ import { ProductImagesComponent } from './components/product-view/product-images
 
 export const routes: Routes = [
     // {path: '**', redirectTo: ''},
-    { path: '', component: ProductListComponent },
-    {path: 'products/:id', component: ProductViewComponent},
-    {path: 'cart', component: CartComponent},
-    { path: 'product-create', component: ProductCreateComponent },
-    {path: 'checkout', component: CheckoutComponent},
-    {path: 'product-images/:id', component: ProductImagesComponent},
+    { path: '', loadComponent: () => import('./components/product-list/product-list.component').then(m => m.ProductListComponent)},
+    {path: 'products/:id', loadComponent: () => import('./components/product-view/product-view.component').then(m => m.ProductViewComponent)},
+    {path: 'cart', loadComponent: () => import('./components/cart/cart.component').then(m => m.CartComponent)},
+    { path: 'product-create', loadComponent: () => import('./components/product-create/product-create.component').then(m => m.ProductCreateComponent)},
+    {path: 'checkout', loadComponent: () => import('./components/checkout/checkout.component').then(m => m.CheckoutComponent)},
+    {path: 'product-images/:id', loadComponent: () => import('./components/product-view/product-images/product-images.component').then(m => m.ProductImagesComponent)},
 ];
