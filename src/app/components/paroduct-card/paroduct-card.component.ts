04234757578acf04b86b151c74ba63d4e27092a5
Ahmed Al-Majidi <67476296+AhmedAl-Majidi@users.noreply.github.com>
2024-09-10 10:06:17 +0300

    Update: replace discount percentage by old price in card component

--- src/app/components/paroduct-card/paroduct-card.component.ts
+++ src/app/components/paroduct-card/paroduct-card.component.ts
@@ -6,41 +6,43 @@ import { Product } from '../../models/product';
 import { CartService } from '../../services/cart.service';
 import { ProductService } from '../../services/product.service';
 
-// 
+//
 @Component({
   selector: 'app-paroduct-card',
   standalone: true,
   imports: [NgxStarsModule, CommonModule],
   templateUrl: './paroduct-card.component.html',
-  styleUrl: './paroduct-card.component.css'
+  styleUrl: './paroduct-card.component.css',
 })
 export class ParoductCardComponent {
-
   product!: Product;
-  // 
-  @Input () InProduct!: Product;
+  //
+  @Input() InProduct!: Product;
 
+  oldPrice: number = 0;
 
   constructor(
     private router: Router,
     private cartService: CartService,
     private productService: ProductService
-    ){}
+  ) {}
 
   ngOnInit(): void {
-
     this.productService.getProductById(this.InProduct.id).subscribe({
       next: (product) => {
         this.product = product;
-      }
+      },
     });
+
+    this.oldPrice =
+      this.InProduct.price / (1 - this.InProduct.discountPercentage / 100);
   }
   // Routing to product details pag
   viewDetails(id: number): void {
     this.router.navigate(['/products', id]);
   }
 
-  // 
+  //
   toggleReadMore(product: any) {
     product.showMore = !product.showMore;
   }
